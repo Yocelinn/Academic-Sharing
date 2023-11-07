14ae5dc058ea417ad9910c26c0c21ba0350c5067
李扬 <21373135>
2023-11-07 20:09:30 +0800

    net

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as echarts from 'echarts'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.config.globalProperties.$echarts = echarts
+app.use(store).use(router).mount('#app')
