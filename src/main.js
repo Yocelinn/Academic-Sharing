bc40a4dddc3ffc0de0aed3bfc530b093de47338a
ignisters <111102376+ignisters@users.noreply.github.com>
2023-11-29 18:03:22 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,10 +2,39 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
-const app= createApp(App)
-app.use(store)
-app.use(router)
-app.use(ElementPlus)
-app.mount('#app')
+import * as echarts from 'echarts'
+// import 'element-plus/dist/index.css';
+import './assets/element.scss'
+import './assets/css/global.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/theme-chalk/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+// 康家麒加的
+const debounce = (fn, delay) => {
+  let timer = null;
+  return function () {
+  let context = this;
+  let args = arguments;
+  clearTimeout(timer);
+  timer = setTimeout(function () {
+      fn.apply(context, args);
+    }, delay);
+  }
+}
+const _ResizeObserver = window.ResizeObserver;
+window.ResizeObserver = class ResizeObserver extends _ResizeObserver{
+  constructor(callback) {
+      callback = debounce(callback, 16);
+      super(callback);
+  }
+}
+
+// 
+const app = createApp(App)
+app.use(store).use(router).use(ElementPlus).mount('#app')
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.config.globalProperties.$echarts = echarts
