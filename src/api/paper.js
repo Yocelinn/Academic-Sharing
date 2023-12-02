66a04141c0093adc8823f81fbf58adf920aeda58
JeruisiAlex <1525772623@qq.com>
2023-12-02 14:22:52 +0800

    20231202 1422 Jeruisi

---
+++ src/api/paper.js
@@ -0,0 +1,18 @@
+import { post, get } from "./api";
+
+
+export function GetCommentByWorkId(workId){
+    return get('/comment/getByWorkId/?workId='+workId);
+}
+
+export function DisplayWorkHomePage(workId){
+    return get('/works/displayWorkHomePage/?workID'+workId)
+}
+
+export function GetAuthorIdByWorkId(workId){
+    return get('/author/getAuthorIdByWorkId/?workId='+workId)
+}
+
+export function GetReferenceById(workId){
+    return get('/works/getReferenceById/?workId='+workId)
+}
