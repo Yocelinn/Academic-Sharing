964c9a124ec4b79554c3263056fe1319d7b41e1a
ignisters <111102376+ignisters@users.noreply.github.com>
2023-12-23 22:39:46 +0800

    updata

--- src/api/record.js
+++ src/api/record.js
@@ -1,1 +1,22 @@
 import { post, get } from './api.js';
+export function GetHistory(){
+    let param={}
+    return get("/record/display",param)
+}
+
+export function DeleteHistory(id){
+    let param={}
+    param.id=id
+    return get("/record/delete",param)
+}
+
+export function SearchHistory(keyword){
+    let param={}
+    param.keyword=keyword
+    return get("/record/filter",param)
+}
+
+export function GetData(){
+    let param={}
+    return get("/record/getConcept",param)
+}
