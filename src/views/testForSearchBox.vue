8e445e1165d1ea40b90826b04fe7dea30687ca5e
plutokang <1420246734@qq.com>
2023-12-23 21:36:02 +0800

    1223 2135 pluto

--- src/views/testForSearchBox.vue
+++ src/views/testForSearchBox.vue
@@ -2,7 +2,7 @@
     <div class="mainContainer" style="width: 1000px;">
         <!-- <el-button style="margin-top: 100px;" @click="isDialoVisibal = true">test</el-button>
         <seniorSearchBox v-model="isDialoVisibal"></seniorSearchBox> -->
-        <searchBox width=40 color="white" :isClassVisible=false  :isLargeModel = false></searchBox>
+        <searchBox width=100 color="white" :isClassVisible=false  :isLargeModel = true></searchBox>
         <el-button @click="test1">test</el-button>
         <!-- <button @click="test">test</button> -->
     </div>
